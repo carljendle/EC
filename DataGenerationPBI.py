ae273a236cb54415733c9949c96aed3b36abbcf1
carljendle <36036870+carljendle@users.noreply.github.com>
2021-10-17 22:10:53 +0200

    Update DataGenerationPBI.py

--- DataGenerationPBI.py
+++ DataGenerationPBI.py
@@ -1,15 +1,20 @@
 import pandas as pd
 from data_gen_utils import *
 
-
-
-for country in countries:
-    csv = []
-    for day in days:
-        for i in range(10):
-            csv.append(generate_data(country, day, country_information, meat_information, relationships))
-    df = pd.DataFrame(csv, columns = cols)
-    df.to_csv(str(country + ".csv"), index = False)
+def main():
+    for country in countries:
+        csv = []
+        for day in days:
+            for i in range(10):
+                csv.append(generate_data(country, day, country_information, meat_information, relationships))
+        df = pd.DataFrame(csv, columns = cols)
+        df.to_csv(str(country + ".csv"), index = False)
+       
+    
+if __name__ == "__main__":
+    main()
+    
+    
     
 
 
