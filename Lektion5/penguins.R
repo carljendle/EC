b605575beadaeb80961a87d5b40b25e52fd644e8
carljendle <36036870+carljendle@users.noreply.github.com>
2021-12-01 12:58:46 +0100

    Update penguins.R

--- Lektion5/penguins.R
+++ Lektion5/penguins.R
@@ -1,7 +1,6 @@
 #https://www.r-bloggers.com/2020/06/penguins-dataset-overview-iris-alternative-in-r/
 #install.packages("remotes")
 #remotes::install_github("allisonhorst/palmerpenguins")
-#https://allisonhorst.github.io/palmerpenguins/articles/articles/pca.html
 
 #
 library(tidyverse)
@@ -13,8 +12,15 @@ library(recipes)
 #install.packages("GGally")
 
 library("ggplot2")                     
-library("GGally")                      
+library("GGally")
 
+library(cluster)
+
+
+
+######################
+# Snabbanalys av datan
+######################
 print(names(penguins))
 
 df <- as.tibble(penguins)
@@ -41,6 +47,12 @@ print(summary(non_na_df))
 print(describe(non_na_df))
 
 
+
+
+######################
+# Visualisering
+######################
+
 non_na_df %>%
   select(species, where(is.numeric)) %>% 
   GGally::ggpairs(aes(color = species),
@@ -50,7 +62,9 @@ non_na_df %>%
   scale_fill_manual(values = c("darkorange","purple","cyan4"))
 
 
-
+######################
+# Principal Component Analysis
+######################
 penguin_recipe <-
   recipe(~., data = non_na_df) %>% 
   update_role(species, island, sex, new_role = "id") %>% 
@@ -82,6 +96,39 @@ penguin_recipe %>%
   xlim(c(0, 5)) + 
   ylab("% of total variance")
 
+
+##############################
+#Klustring - innan normalisering
+#############################
+
+labels <- non_na_df$species
+
+kmeans_data <- non_na_df %>% select(where(is.numeric))
+
+min_val <- 2
+max_val <- 10
+results <- as.tibble(cbind("Measure" = 0, "Clusters" = 0))
+
+
+for( i in min_val:max_val){
+  kmeans_results <- kmeans(kmeans_data, centers = i, nstart = 20)
+  
+  measure <- kmeans_results$betweenss/kmeans.re$totss
+  
+  results <- rbind(results, list(measure, i))
+  
+}
+
+
+print(kmeans_results)
+#För att inspektera vad vi har i vår kmeans.re - använd str()
+print(str(kmeans_results))
+
+#Plocka bort startvärde för 0 kluster - inte superspännande!
+results <- results[-1, ]
+ggplot(data=results[-1, ], aes(x=Clusters, y=Measure)) +
+  geom_line()+
+  geom_point()
 
 
 
